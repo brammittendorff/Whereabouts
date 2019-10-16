c44087510f4a4738cb32a2728f4a7ee6be10e048
Pipaolo <mastastah8975@gmail.com>
2019-10-17 01:15:22 +0800

    Revert "Reworked IOs compat"
    This reverts commit 06bb8b79177a45b21d2f7b2900a41a0b36b02f5a.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,6 @@
 import UIKit
 import Flutter
 import GoogleMaps
-import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,8 +8,7 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBXcT0Rx47h3xAChxrSzxBKBhuMzK9jjFA")
-    FirebaseApp.configure()
+    GMSServices.provideAPIKEY("AIzaSyBXcT0Rx47h3xAChxrSzxBKBhuMzK9jjFA")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
